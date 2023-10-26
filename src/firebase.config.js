bf60ed1fd358c3234ebadf8a907e0355a0460db1
Carlos Rangel <charlesleimaginant@gmail.com>
2023-10-26 17:05:06 -0600

    modify config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -14,12 +14,12 @@ const FIREBASE_APP_ID= import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID
+  apiKey: "AIzaSyDucuXybPInKzv6kUdCtgILJSCbeyEuOXs",
+  authDomain: "house-marketplace-app-53bfe.firebaseapp.com",
+  projectId: "house-marketplace-app-53bfe",
+  storageBucket: "house-marketplace-app-53bfe.appspot.com",
+  messagingSenderId: "892640687437",
+  appId: "1:892640687437:web:ef6c2b2fea4258e62b7304"
 };
 
 // Initialize Firebase
